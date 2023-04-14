98e59e8ba42c4c28c65c5db50dac06a6289ea778
LucasMiranda <lucaswespras@gmail.com>
2023-04-13 21:35:24 -0300

    inicio

---
+++ src/test/java/com/primeirotrampodev/ListaDePessoa/ListaDePessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.ListaDePessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDePessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
